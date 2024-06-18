d1af5b1bba974199aac6687c122a8e0ee491e5db
DeepSheth221202 <deep.sheth@etatvasoft.com>
2024-06-18 15:09:26 +0530

    authentication done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,7 @@ import {
   Delete,
   NotFoundException,
   Session,
-  UseInterceptors
+  UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -18,12 +18,11 @@ import { UserDto } from 'src/users/dtos/user.dto';
 import {Serialize} from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(private readonly usersservice: UsersService, private readonly authService : AuthService) {}
 
@@ -47,6 +46,7 @@ export class UsersController {
 //   }
 
   @Get('/whoami')
+  @UseGuards(AuthGuard)
   whoAmI(@CurrentUser() user : User){
     return user;
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,9 +6,16 @@ import { User } from './user.entity';
 import {AuthService} from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
+import { APP_INTERCEPTOR } from '@nestjs/core'; //global interceptor
+
 @Module({
   imports:[TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService,AuthService, CurrentUserInterceptor]
+  providers: [UsersService,AuthService, 
+    {
+      provide : APP_INTERCEPTOR,
+      useClass : CurrentUserInterceptor
+    } //setup global scope interceptor which is applied on requests from all controllers
+  ]
 })
 export class UsersModule {}
